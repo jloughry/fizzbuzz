015ee4df18ecd6170131cb0c22373b17e50f8a54
jloughry <joe.loughry@stx.ox.ac.uk>
2013-11-18 11:05:49 -0700

    commit from Makefile 20131118.1105

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -3,9 +3,13 @@
 
 int fizzbuzz(int);
 
-/* Handle the words in the obvious way (using fallthrough), but also
-   return the product of the modular residues.  Let the compiler optimize
-   out the common subexpression.  The goal here is clarity of expression. */
+/* Handle the words in the obvious way (using fallthrough), but at the same
+   time return the product of the modular residues. Let the compiler optimize
+   out the common subexpression. The goal here is clarity of expression. The
+   function has benign side effects---printing the words---but is otherwise
+   pure functional. It's extensible, but I didn't add the extra functionality
+   for help, USAGE, command line arguments to make it work for any $n$ (with
+   sanitisation of user input of course) or 64-bit integers because YAGNI. */
 
 int fizzbuzz(int i) {
 	if (!(i % 3)) printf("fizz");
@@ -13,7 +17,7 @@ int fizzbuzz(int i) {
 	return i%3 * i%5;
 }
 
-/* If the function is anything other than zero, display the number. */
+/* If the function returns anything other than zero, print the number. */
 
 int main(void) {
 	int i=0;
